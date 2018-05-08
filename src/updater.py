e9c068f22aaa1a1d2af4641134af6441c7f95b5e
wsbespalov <ws.bespalov@gmail.com>
2018-05-08 17:07:11 +0500

    complete fast search with redis and postgras M->S repl.

--- src/updater.py
+++ src/updater.py
@@ -1,10 +1,13 @@
 import sys
 import re
-import pika
 import ast
-import peewee
+import pika
 import json
 import time
+import redis
+import urllib
+import peewee
+import string
 from dateutil.parser import parse as parse_datetime
 from datetime import datetime
 import cpe as cpe_module
@@ -36,6 +39,14 @@ SETTINGS = {
         "drop_before": True,
         "cache_size_mb": 64
     },
+    "cache": {
+        "host": 'localhost',
+        "port": 6379,
+        "db": 3,
+        "separator": "::",
+        "index": "index",
+        "key_expire_time_in_sec": 60 # 1 minute for tests
+    },
     "debug": True
 }
 
@@ -48,6 +59,11 @@ database = peewee.PostgresqlDatabase(
     password=POSTGRES.get("password", "password"),
     host=POSTGRES.get("host", "localhost"),
     port=int(POSTGRES.get("port", 5432))
+)
+cache = redis.StrictRedis(
+    host=SETTINGS["cache"]["host"],
+    port=SETTINGS["cache"]["port"],
+    db=SETTINGS["cache"]["db"]
 )
 
 def connect_database():
@@ -236,10 +252,10 @@ def print_short(items_to_print):
             item["cpe"],
             item["vulnerable_configuration"]["data"]))
 
-def serialize_json(source):
+def serialize_json__for_postgres(source):
     return json.dumps(ast.literal_eval(source))
 
-def deserialize_json(source):
+def deserialize_json__for_postgres(source):
     a = ast.literal_eval(source)
     if isinstance(a, dict):
         return a
@@ -260,10 +276,6 @@ def progressbar(it, prefix="Processing ", size=50):
         _show(i + 1)
     sys.stdout.write("\n")
     sys.stdout.flush()
-
-def normalize_ts(first_ts, second_ts):
-
-    pass
 
 ##############################################################################
 
@@ -290,7 +302,7 @@ def filter_cpe_string__json(element):
 
     return result
 
-def filter_items_to_update__list_of_items(items_fo_filter):
+def filter_items_to_update__list_of_items(items_fo_filter, unquote=True, only_digits_and_dot_in_version=True):
     filtered_items = []
     # for item in items_fo_filter:
     for item in progressbar(items_fo_filter, prefix='Filtering  '):
@@ -313,6 +325,14 @@ def filter_items_to_update__list_of_items(items_fo_filter):
                         new_item = item.copy()
                         new_item["component"] = filtered_cpe_string["component"]
                         new_item["version"] = filtered_cpe_string["version"]
+                        if unquote:
+                            try:
+                                new_item["version"] = urllib.parse.unquote(new_item["version"])
+                            except:
+                                pass
+                        if only_digits_and_dot_in_version:
+                            allow = string.digits + '.' + '(' + ')'
+                            new_item["version"] = re.sub('[^%s]' % allow, '', new_item["version"])
                         new_item["vulnerable_configuration"] = {"data": list_of_cpe_ctrings}
                         new_item["cpe"] = one_cpe_string
                         filtered_items.append(new_item)
@@ -367,21 +387,21 @@ def update_vulner_in_database__vulner_id(item_to_update, item_id_in_database):
     if vulner["data_version"] != item_to_update["data_version"]:
         was_modified = True
         vulner_from_database.data_version = item_to_update["data_version"]
-    if deserialize_json(vulner["cwe"]) != item_to_update["cwe"]:
+    if deserialize_json__for_postgres(vulner["cwe"]) != item_to_update["cwe"]:
         was_modified = True
-        vulner_from_database.cwe = serialize_json(item_to_update["cwe"])
-    if deserialize_json(vulner["references"]) != item_to_update["references"]:
+        vulner_from_database.cwe = serialize_json__for_postgres(item_to_update["cwe"])
+    if deserialize_json__for_postgres(vulner["references"]) != item_to_update["references"]:
         was_modified = True
-        vulner_from_database.references = serialize_json(item_to_update["references"])
+        vulner_from_database.references = serialize_json__for_postgres(item_to_update["references"])
     if vulner["description"] != item_to_update["description"]:
         was_modified = True
         vulner_from_database.description = item_to_update["description"]
     if vulner["cpe"] != item_to_update["cpe"]:
         was_modified = True
         vulner_from_database.cpe = item_to_update["cpe"]
-    if deserialize_json(vulner["vulnerable_configuration"]) != item_to_update["vulnerable_configuration"]:
+    if deserialize_json__for_postgres(vulner["vulnerable_configuration"]) != item_to_update["vulnerable_configuration"]:
         was_modified = True
-        vulner_from_database.vulnerable_configuration = serialize_json(item_to_update["vulnerable_configuration"])
+        vulner_from_database.vulnerable_configuration = serialize_json__for_postgres(item_to_update["vulnerable_configuration"])
     if unify_time(vulner["published"]) != unify_time(item_to_update["published"]):
         was_modified = True
         vulner_from_database.published = unify_time(item_to_update["published"])
@@ -483,7 +503,7 @@ def update_recent_cves_from_source__counts():
 
 ##############################################################################
 
-def find_vulner_in_postgres_by_cve_id__list_of_items(cve_id):
+def find_vulner_in_postgres_by_cve_id__list_of_items_in_json(cve_id):
     connect_database()
     database_items = list(vulnerabilities.select().where(vulnerabilities.cve_id==cve_id))
     items = []
@@ -494,7 +514,7 @@ def find_vulner_in_postgres_by_cve_id__list_of_items(cve_id):
     disconnect_database()
     return items
 
-def find_list_of_vulners_in_postgres_by_cve_id__list_of_items(list_of_cve_ids):
+def find_list_of_vulners_in_postgres_by_cve_id__list_of_items_in_json(list_of_cve_ids):
     items = []
     if isinstance(list_of_cve_ids, list):
         connect_database()
@@ -505,7 +525,7 @@ def find_list_of_vulners_in_postgres_by_cve_id__list_of_items(list_of_cve_ids):
         disconnect_database()
     return items
 
-def find_vulner_in_postgres_by_component_and_version__list_of_items(component, version):
+def find_vulners_in_postgres_by_component_and_version__list_of_items_in_json(component, version):
     connect_database()
     list_of_elements = []
     if "*" in version:
@@ -529,7 +549,7 @@ def find_vulner_in_postgres_by_component_and_version__list_of_items(component, v
     disconnect_database()
     return items
 
-def find_list_of_vulners_in_postgres_by_component_and_versions_list__list_of_items(list_of_component_and_versions):
+def find_list_of_vulners_in_postgres_by_component_and_versions_list__list_of_items_in_json(list_of_component_and_versions):
     """
     Get list if vulners with component and versions.
     If version contains "*" - slice version and use "startwith".
@@ -546,7 +566,6 @@ def find_list_of_vulners_in_postgres_by_component_and_versions_list__list_of_ite
                 component = str(component_and_version["component"])
                 version = str(component_and_version["version"])
                 list_of_elements = []
-
                 if "*" in version:
                     version = version[:version.index("*")]
                     list_of_elements = list(
@@ -568,6 +587,109 @@ def find_list_of_vulners_in_postgres_by_component_and_versions_list__list_of_ite
                 items = items + new_items
         disconnect_database()
     return items
+
+##############################################################################
+
+def serialize_as_json__for_cache(element):
+    def dt_converter(o):
+        if isinstance(o, datetime):
+            return o.__str__()
+    try:
+        return json.dumps(element, default=dt_converter)
+    except Exception as ex:
+        print("{}".format(ex))
+        return None
+
+def deserialize_as_json__for_cache(element):
+    try:
+        return json.loads(element)
+    except:
+        return None
+
+def create_collection_name_by_component_and_version(component, version):
+    if version is None:
+        version = "*"
+    if version == "":
+        version = "*"
+    collection_name = "".join([
+        SETTINGS["cache"]["index"],
+        SETTINGS["cache"]["separator"],
+        component,
+        SETTINGS["cache"]["separator"],
+        str(version)
+    ])
+    return collection_name
+
+def put_items_into_redis_cache(items_to_cache):
+    count = 0
+    for element in items_to_cache:
+        component = element.get("component", None)
+        version = element.get("version", None)
+        if component is not None and version is not None:
+            collection_name = create_collection_name_by_component_and_version(
+                component=component,
+                version=version
+            )
+            try:
+                cache.rpush(
+                    collection_name,
+                    serialize_as_json__for_cache(
+                        element=element
+                    )
+                )
+                cache.expire(
+                    collection_name,
+                    SETTINGS["cache"]["key_expire_time_in_sec"]
+                )
+                count += 1
+            except Exception as ex:
+                print("{}".format(ex))
+    return count
+
+def check_if_item_is_already_cached_in_redis__list_of_items_in_json(component, version):
+    collection_name = create_collection_name_by_component_and_version(
+        component=component,
+        version=version
+    )
+    elements_in_cache = []
+    try:
+        elements_in_cache = cache.lrange(
+            collection_name, 0, -1
+        )
+    except:
+        pass
+    list_of_components = []
+    for element in elements_in_cache:
+        list_of_components.append(
+            deserialize_as_json__for_cache(
+                element
+            )
+        )
+    return list_of_components
+
+def fast_search_for_list_of_vulners__list_of_items_as_json(list_of_component_and_versions):
+    ready_items = []
+    for item in list_of_component_and_versions:
+        if isinstance(item, dict):
+            component = item.get("component", None)
+            version = item.get("version", None)
+            if component is not None and version is not None:
+                items_in_redis = check_if_item_is_already_cached_in_redis__list_of_items_in_json(component, version)
+                if len(items_in_redis) > 0:
+                    # If item in redis - get data from redis
+                    ready_items = ready_items + items_in_redis
+                else:
+                    # If item not in redis - get data from postgres
+                    items_in_postgres = find_vulners_in_postgres_by_component_and_version__list_of_items_in_json(
+                        component=component,
+                        version=version
+                    )
+                    # And put it into cache
+                    put_items_into_redis_cache(items_in_postgres)
+                    # Append result
+                    ready_items = ready_items + items_in_postgres
+    return ready_items
+
 
 def only_digits(var):
     return re.sub("\D", "", var)
@@ -667,11 +789,19 @@ def main():
     # ]))
     # print("TimeDelta: {}".format(time.time() - start_time))
 
+    # start_time = time.time()
+    # print_list(find_list_of_vulners_in_postgres_by_component_and_versions_list__list_of_items([
+    #     {"component": "ffmpeg", "version": "3.2*"},
+    # ]))
+    # print("TimeDelta: {}".format(time.time() - start_time))
+
     start_time = time.time()
-    print_list(find_list_of_vulners_in_postgres_by_component_and_versions_list__list_of_items([
-        {"component": "ffmpeg", "version": "3.2*"},
+    print_list(fast_search_for_list_of_vulners__list_of_items_as_json([
+        {"component": "junos", "version": "14.*"},
     ]))
     print("TimeDelta: {}".format(time.time() - start_time))
+
+    pass
 
 
 ##############################################################################
