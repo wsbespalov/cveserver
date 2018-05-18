41ef743d1a9adca5e307034949c46cd05638b4f5
ws.shkurko <imac@iMac-iMac.local>
2018-05-18 14:32:53 +0300

    populater object queue

--- src/populater.py
+++ src/populater.py
@@ -34,32 +34,81 @@ def run():
             if message_to_get_vulnerability in data:
                 id_of_request = data[len(message_to_get_vulnerability) + 10:]
                 uniq_id = data[len(message_to_get_vulnerability) + 1 :len(message_to_get_vulnerability) + 9]
-                print('uniq', uniq_id)
-                print('id_of_request', id_of_request)
-                new_collection_name = SETTINGS["queue"]["complete_get_vulnerability"] + uniq_id + ':' + id_of_request
-                connect_database()
-                vulnerability = list(
-                    vulnerabilities.select().where(
-                        vulnerabilities.id == id_of_request
-                    )
-                )
-                vlist = []
-                for i in vulnerability:
-                    vlist.append(i.to_json)
-                if len(vlist) == 1:
+                # print('uniq', uniq_id)
+                # print('id_of_request', id_of_request)
+                try:
+                    int(id_of_request)
+                except ValueError:
+                    search_options = json.loads(id_of_request)
+                    new_collection_name = SETTINGS["queue"]["complete_get_vulnerability"] + uniq_id + ':' + search_options['name'] + ':' + search_options['version']
+
+                    search_options['name'] = 'junos'
+                    search_options['version'] = '*'
+
+                    connect_database()
+                    if search_options['version'] == '*':
+                        count = vulnerabilities.select().where(
+                                vulnerabilities.component == search_options['name']
+                            ).count()
+                        vulnerability = list(
+                            vulnerabilities.select().where(
+                                    (vulnerabilities.component == search_options['name']) &
+                                    (vulnerabilities.cvss >= search_options['sort'])
+                            ).offset(search_options['skip']).limit(search_options['limit']).order_by(vulnerabilities.cvss.desc())
+                        )
+                    else:
+                        count = vulnerabilities.select().where(
+                                (vulnerabilities.component == search_options['name']) &
+                                (vulnerabilities.version == search_options['version'])
+                            ).count()
+                        vulnerability = list(
+                            vulnerabilities.select().where(
+                                (vulnerabilities.component == search_options['name']) &
+                                (vulnerabilities.version == search_options['version']) &
+                                (vulnerabilities.cvss >= search_options['sort'])
+                            ).offset(search_options['skip']).limit(search_options['limit']).order_by(vulnerabilities.cvss.desc())
+                        )
                     queue.rpush(
                         new_collection_name,
-                        serialize_as_json__for_cache(reformat_vulner_for_output(vlist[0]))
+                        count
                     )
+                    for i in vulnerability:
+                        queue.rpush(
+                            new_collection_name,
+                            serialize_as_json__for_cache(reformat_vulner_for_output(i.to_json))
+                        )
+                    disconnect_database()
+                    queue.publish(
+                        channel=channel_to_subscribe_and_publish,
+                        message=new_collection_name
+                    )
+                except Exception as ex:
+                    print(ex)
                 else:
-                    pass
-                disconnect_database()
-                # complete_message = SETTINGS["queue"]["complete_get_vulnerability"] + id_of_request
-                queue.publish(
-                    channel=channel_to_subscribe_and_publish,
-                    message=new_collection_name
-                    # message=complete_message
-                )
+                    new_collection_name = SETTINGS["queue"]["complete_get_vulnerability"] + uniq_id + ':' + id_of_request
+                    connect_database()
+                    vulnerability = list(
+                        vulnerabilities.select().where(
+                            vulnerabilities.id == id_of_request
+                        )
+                    )
+                    vlist = []
+                    for i in vulnerability:
+                        vlist.append(i.to_json)
+                    if len(vlist) == 1:
+                        queue.rpush(
+                            new_collection_name,
+                            serialize_as_json__for_cache(reformat_vulner_for_output(vlist[0]))
+                        )
+                    else:
+                        pass
+                    disconnect_database()
+                    # complete_message = SETTINGS["queue"]["complete_get_vulnerability"] + id_of_request
+                    queue.publish(
+                        channel=channel_to_subscribe_and_publish,
+                        message=new_collection_name
+                        # message=complete_message
+                    )
             else:
                 pass
     pass
